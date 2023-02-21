12e7e7af6a625c73ea3473398bc4a3014050fa9b
ericstefano <ericstefano12@gmail.com>
2023-02-21 15:47:18 -0300

    fix: apply tailwind css to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import { ReactNode } from "react";
+import '@/styles/main.css';
+import { ReactNode } from 'react';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
